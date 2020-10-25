6e00a9cb662b610086faad26a2ab439cd3894d45
Catherine Gloria <catherinegloria@gmail.com>
2020-10-25 18:10:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,9 +12,9 @@ class Triangle
     if a == 0 || b == 0 || c == 0
       raise TriangleError
     elsif a.negative?|| b.negative? || c.negative?
-        raise TriangleError
+      raise TriangleError
     elsif (a + b) <= c || (a + c) <= b || (b + c) <= a
-        raise TriangleError
+      raise TriangleError
     elsif a == b && a == c
       :equilateral
     elsif a == b && a != c || a == c && a != b || b == c && b != a
